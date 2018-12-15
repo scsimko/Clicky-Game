68152a6dd98a34497aa9c08fd8eece686de0db78
scsimko <stephensimko.bootcamp@gmail.com>
2018-12-14 21:25:06 -0500

    Image add

---
+++ my-app/src/assets/images.js
@@ -0,0 +1,64 @@
+const images = [
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/k2ivkztquyjcvlgyrkoz',
+      alt: 'Nick Chubb'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/m5w7xosrx8o5orwhvo84',
+      alt: 'Myles Garrett'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/zw8f3y2uhu6d0i2yck4w',
+      alt: 'Jarvis Landry'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/dmtxusya1elk1o6jxi2z',
+      alt: 'Baker Mayfield'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/vme5uqswiytjxhtvqkk5',
+      alt: 'David Njoku'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/rxehipcraqcpw2bbdytu',
+      alt: 'Emmanuel Ogbah'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/u4wp8ujgzs9h0byf0bw2',
+      alt: 'Jabrill Peppers'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/zaqk27d1is8u7rrhzkdd',
+      alt: 'Damarious Randall'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/sfk4kbm9d1v9pduerrnk',
+      alt: 'Antonio Callaway'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/wnhrlpdajsvmo9b0bqje',
+      alt: 'Denzel Ward'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/t3nnpwsrzehxvloeuugc',
+      alt: 'Duke Johnson'
+    },
+    {
+      clicked: false,
+      src: 'https://static.clubs.nfl.com/image/private/t_editorial_landscape_6_desktop_2x/f_auto/browns/wipanaxyt5gps6ml4yvv',
+      alt: 'Rashard Higgins'
+    },
+  ]
+  
+  export default images;
